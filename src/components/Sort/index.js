6fc48fc0c17b07bd06faf14c7165207944f654f5
YantongLi123 <yxl2242@case.edu>
2021-07-14 18:40:40 -0500

    Sort Component

--- src/components/Sort/index.js
+++ src/components/Sort/index.js
@@ -1,24 +1,57 @@
 import { Menu, Dropdown, Button} from 'antd'
 const { SubMenu } = Menu;
 
-const Sort = () => {
+const Sort = ({sortType,setSortType,setCurPage}) => {
+    const handlePDClick = () => {
+        setSortType('Popularity Descending');
+        setCurPage(1);
+    }
+    const handlePAClick = () => {
+        setSortType('Popularity Aescending');
+        setCurPage(1);
+    }
+    const handleRDClick = () => {
+        setSortType('Rating Descending');
+        setCurPage(1);
+    }
+    const handleRAClick = () => {
+        setSortType('Rating Ascending');
+        setCurPage(1);
+    }
+    const handleRDDClick = () => {
+        setSortType('Release Date Descending');
+        setCurPage(1);
+    }
+    const handleRDAClick = () => {
+        setSortType('Release Data Ascending');
+        setCurPage(1);
+    }
+    const handleAZClick = () => {
+        setSortType('Title (A-Z)');
+        setCurPage(1);
+    }
+    const handleZAClick = () => {
+        setSortType('Title (Z-A)');
+        setCurPage(1);
+    }
     const menu = (
         <Menu>
-            <SubMenu title='Sort Results By'>
-                <Menu.Item>Popularity Descending</Menu.Item>
-                <Menu.Item>Popularity Descending</Menu.Item>
-                <Menu.Item>Rating Descending</Menu.Item>
-                <Menu.Item>Rating Ascending</Menu.Item>
-                <Menu.Item>Release Date Descending</Menu.Item>
-                <Menu.Item>Release Data Ascending</Menu.Item>
-                <Menu.Item>Title (A-Z)</Menu.Item>
-                <Menu.Item>Title (Z-A)</Menu.Item>
+            <SubMenu key = 'subtitle' title='Sort Results By'>
+                <Menu.Item key='Popularity Descending' onClick={handlePDClick}>Popularity Descending</Menu.Item>
+                <Menu.Item key='Popularity Ascending'onClick={handlePAClick}>Popularity Ascending</Menu.Item>
+                <Menu.Item key='Rating Descending' onClick={handleRDClick}>Rating Descending</Menu.Item>
+                <Menu.Item key='Rating Ascending' onClick={handleRAClick}>Rating Ascending</Menu.Item>
+                <Menu.Item key='Release Date Descending' onClick={handleRDDClick}>Release Date Descending</Menu.Item>
+                <Menu.Item key='Release Data Ascending' onClick={handleRDAClick}>Release Data Ascending</Menu.Item>
+                <Menu.Item key='Title (A-Z)' onClick={handleAZClick}>Title (A-Z)</Menu.Item>
+                <Menu.Item key='Title (Z-A)' onClick={handleZAClick}>Title (Z-A)</Menu.Item>
             </SubMenu>
         </Menu>
     );
+    
     return(
         <Dropdown overlay={menu}>
-            <Button>Filter</Button>
+            <Button>{sortType}</Button>
         </Dropdown>
     )
 }
