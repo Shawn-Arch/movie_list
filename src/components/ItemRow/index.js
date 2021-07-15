59b5bfda5b4ee487e882c651c5ab9733f565ca7e
Xiaochen <xcgao.shawn@gmail.com>
2021-07-15 10:37:05 -0500

    Adding transition animation

--- src/components/ItemRow/index.js
+++ src/components/ItemRow/index.js
@@ -6,23 +6,23 @@ import PeopleCard from '../PeopleCard';
 
 import "./index.scss"
 
-const ItemRow = ({type, title, list}) => {
-    const [startIndex, setStartIndex] = useState(0);
+const ItemRow = ({type, title, maxPage,list}) => {
+    const [curPage, setCurPage] = useState(0);
     if (!Array.isArray(list)) return(<></>);
 
     const handleRightClick = ()=>{
-        setStartIndex(startIndex+7)
+        setCurPage(curPage+1)
     }
     const handleLeftClick = ()=>{
-        setStartIndex(startIndex-7)
+        setCurPage(curPage-1)
     }
     return (
     <>
         <p className="list-title">{title}</p>
         <div className="container">
-            {/* {startIndex !== 0 && <LeftOutlined className="icon left-icon" onClick={handleLeftClick}/>} */}
-            {/* {startIndex !== 28 && <RightOutlined className="icon right-icon" onClick={handleRightClick}/>} */}
-            <Row gutter={16} wrap={false}>
+            {<LeftOutlined className="icon left-icon" onClick={handleLeftClick}/>}
+            {<RightOutlined className="icon right-icon" onClick={handleRightClick}/>}
+            <Row gutter={16} wrap={false} style={{right: curPage+"00%", transition: "right 1s"}}>
                 {list.map((item)=>{
                     return (
                         <Col span={4} key={new Date() + Math.random()}>
