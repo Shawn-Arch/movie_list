afe1b9e881576b0675db1f17de53a581a0ac0ac5
YantongLi123 <yxl2242@case.edu>
2021-07-16 10:36:18 -0500

    Movie details side content

--- src/containers/MovieDetailsPage/index.js
+++ src/containers/MovieDetailsPage/index.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import { APIs } from "../../constant/constant"
 import { movie, person } from "../../util/data";
 import ItemRow from "../../components/ItemRow";
+import MovieDetailsSideContent from "../../components/MovieDetailsSideContent"
 const { Header, Footer, Sider, Content } = Layout;
 
 
@@ -12,6 +13,12 @@ const MovieDetailsPage = (props) => {
     const {id} = useParams();
     const [movieDetails, setMovieDetails] = useState(null);
     const [movieCast, setMovieCast] = useState(null);
+    const [sideContent, setSideContent] = useState({
+        status:'',
+        spoken_languages:[],
+        budget:'',
+        revenue:''
+    })
     const fetchDetails = () => {
         fetch(APIs.DETAILS_MOVIE_PART1 + id + APIs.DETAILS_MOVIE_PART2)
             .then(res=>res.json())
@@ -30,9 +37,28 @@ const MovieDetailsPage = (props) => {
             })
     }
 
+    const fetchSideContent = () => {
+        fetch(APIs.MOVIE_SIDE_CONTENT_PART1+id+APIs.MOVIE_SIDE_CONTENT_PART2)
+            .then(res=>res.json())
+            .then(data=>{
+                // console.log(data.spoken_languages)
+                setSideContent({
+                    status:data.status,
+                    spoken_languages:data.spoken_languages,
+                    budget:data.budget,
+                    revenue:data.revenue
+                })
+            })
+    }
+
+    useEffect(()=>{
+        console.log(sideContent)
+    },[sideContent])
+
     useEffect(()=>{
         fetchDetails();
         fetchCast();
+        fetchSideContent();
     },[])
 
     return (
@@ -44,7 +70,9 @@ const MovieDetailsPage = (props) => {
                 <Content>
                     <ItemRow type="person" list={movieCast} title="Top Billed Cast"/>
                 </Content>
-                <Sider>Sider</Sider>
+                <Sider>
+                    <MovieDetailsSideContent sideContent={sideContent}></MovieDetailsSideContent>
+                </Sider>
             </Layout>
         </Layout>);
 }
